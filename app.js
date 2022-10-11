0b4d840624db0c556fbd5d990d210d7d8f784a2b
INTEL <yousufzubair1234@gmail.com>
2022-10-11 19:30:17 +0500

    Complete the To Do list app

--- app.js
+++ app.js
@@ -1,38 +1,43 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js")
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"))
 app.set('view engine', 'ejs');
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
+let items = [];
+let workItems = [];
 
 app.get("/", function(req, res){
 
-let today = new Date();
-let day = "";
+let day = date();
 
-let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-}
-
-day = today.toLocaleDateString("en-US", options);
-
-res.render("list", {kindOfDay: day, newListItems:items});
+res.render("list", {listTitle: day, newListItems:items});
 
 });
 
 app.post("/", function(req, res){
     let item = req.body.newItem;
 
-    items.push(item);
-
-    res.redirect("/");
+    if(req.body.list === "Work List"){
+        workItems.push(item);
+        res.redirect("/work");
+    }
+    else{
+        items.push(item);
+        res.redirect("/");
+    }
 
 });
+
+// Work page
+
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work List", newListItems:workItems});
+});
+
 
 let port = 5501;
 app.listen(port, function() {
